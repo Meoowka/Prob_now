5cff92d48e05959541a3d2d082ed82d1dfc0d632
Meoowka <sasha.medvedev2017@mail.ru>
2024-03-31 23:12:42 +0300

    q

--- Files.cs
+++ Files.cs
@@ -9,11 +9,15 @@ namespace Prob_now
 {
     public class Files
     {
+        
         public string Name_File { get; set; }
         public string? Versio { get; set; }
         public DateTime? Date { get; set; }
         public string? Autor_ver { get; set; }
         public string Extension { get; set; }
+
+        public string Id_ver { get; set; }
+
        
         public Files(string name_file, string version, DateTime date, string autor_ver, string extension) {
             Name_File = name_file;
@@ -21,6 +25,9 @@ namespace Prob_now
             Date = date;
             Autor_ver = autor_ver;
             Extension = extension;
+        
+            Id_ver = name_file + autor_ver + extension;
+
         }
     }
 }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,20 +1,13 @@
-﻿using System;
+﻿using Aspose.Html.Accessibility;
+using Aspose.Html.Toolkit.Markdown.Syntax;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Configuration;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Prob_now
 {
@@ -29,28 +22,90 @@ namespace Prob_now
         }
         public ObservableCollection<Files> dir_r { get; set; }
 
-        public void recursiveBuildDirectory(DirectoryInfo entrypoint)
+        public void Create_md(List<string> file_md)
         {
-                    foreach (FileInfo FL in entrypoint.GetFiles("*.*", System.IO.SearchOption.AllDirectories))
-                    {
-                        var fvi = FileVersionInfo.GetVersionInfo(FL.FullName);
-                        string? autor_r = fvi.CompanyName;
-                        string? version = fvi.FileVersion;
+            const string file_path = @"E:\";
+            var md = new MarkdownSyntaxTree(new Aspose.Html.Configuration());
+            var mdf = md.SyntaxFactory;
 
-                        var item_file_on_disk = new Files(FL.Name.Split('.')[0], 
-                            version, 
-                            FL.CreationTime, 
+            for (int i = 0; i < file_md.Count; i++)
+            {
+                var head = mdf.AtxHeading(file_md[i] + '\n');
+                md.AppendChild(head);
+            }
+            string savePath = Path.Combine(file_path, "version.md");
+            md.Save(savePath);
+        }
+       public List<Files> list_files = new List<Files>();
+
+        public void Directory_info(DirectoryInfo entrypoint)
+        {
+            List<string> list = new List<string>();
+           
+            foreach (FileInfo FL in entrypoint.GetFiles("*.*", System.IO.SearchOption.AllDirectories))
+            {
+                var fvi = FileVersionInfo.GetVersionInfo(FL.FullName);
+                string? autor_r = fvi.CompanyName;
+                string? version = fvi.FileVersion;
+
+                if (version != null)
+                {
+                    list.Add(version);
+                }
+                
+                var item_file_on_disk = new Files(FL.Name.Split('.')[0],
+                            version,
+                            FL.CreationTime,
                             autor_r,
                             FL.Extension);
-                            List_view.Items.Add(item_file_on_disk); 
-                    }
+                list_files.Add(item_file_on_disk);
+                List_view.Items.Add(item_file_on_disk);
+            }
+            Create_md(list);
         }
-        
+        public void File_info_on_version(DirectoryInfo entrypoint, string ID_inf)
+        {
+            
+            foreach (FileInfo FL in entrypoint.GetFiles("*.*", System.IO.SearchOption.AllDirectories))
+            {
+                var fvi = FileVersionInfo.GetVersionInfo(FL.FullName);
+                string? autor_r = fvi.CompanyName;
+                string? version = fvi.FileVersion;
+                var item_file_on_disk = new Files(FL.Name.Split('.')[0],
+                            version,
+                            FL.CreationTime,
+                            autor_r,
+                            FL.Extension);
+
+                if (ID_inf == item_file_on_disk.Id_ver)
+                {
+                    for (int i = 0; i < list_files.Count; i++)
+                    { 
+                        if (list_files[i].Name_File.Contains(item_file_on_disk.Name_File))
+                        {
+                            List_view_info.Items.Add(item_file_on_disk);
+                        }
+
+                    }
+                }
+
+            }
+
+        }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            DirectoryInfo currentdirectory = new DirectoryInfo(@"E:\\Folder_update");
-            recursiveBuildDirectory(currentdirectory);
+            List_view.Items.Clear();
+            DirectoryInfo currentdirectory = new DirectoryInfo(@"E:\Folder_update");
+            Directory_info(currentdirectory);
+        }
+
+        private void List_view_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            List_view_info.Items.Clear();
+            var point = List_view.SelectedItem as Files;
+            DirectoryInfo currentdirectory = new DirectoryInfo(@"E:\Folder_update"); 
+            File_info_on_version(currentdirectory, point.Id_ver);
         }
     }
 }
